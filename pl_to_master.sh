f4c21a122dc03d59918cfa74fa5f2c1d883dee1f
shinichi-kogai <kogai0121@gmail.com>
2015-03-19 14:45:26 +0900

    mod settings

--- pl_to_master.sh
+++ pl_to_master.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# git request-pull https://github.com/kogai/hyakkiyako master
+# git request-pull https://github.com/kogai/hyakkiyako
+# git request-pull origin/test git@github.com:kogai/hyakkiyako.git origin/master
+# git request-pull test https://github.com/kogai/hyakkiyako/ master
+
 git checkout master
 git merge develop
